0a833b59bfd2d22350ac4adb117bb63a8503e4b2
Dmitriy72 <dmitry72reg@yandex.ru>
2024-05-08 21:23:23 +0500

    modified 1.py

--- 1.py
+++ 1.py
@@ -12,3 +12,18 @@ with open('1.txt', 'r') as file:
                     dish.append({'ingredient_name': ingredient.strip(), 'quantity': int(quantity.strip()), 'measure': measure.strip()})
                 else:
                     dish_name = line
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                if ingredient['ingredient_name'] not in shop_list:
+                    shop_list[ingredient['ingredient_name']] = {
+                        'quantity': ingredient['quantity'] * person_count,
+                        'measure': ingredient['measure']
+                    }
+                else:
+                    shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
+    return shop_list
